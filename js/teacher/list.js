3bdb76698138dee710a5261e0815c7a0e1264aee
doudou <shaofengyuan94723@qq.com>
2017-03-01 11:38:11 +0800

    讲师列表的注销和开启

--- js/teacher/list.js
+++ js/teacher/list.js
@@ -13,8 +13,25 @@ define(['jquery','template','common'],function ($,template,undefined) {
 		$.get('/v6/teacher/view',{tc_id:$(this).parent().attr('data-id')},function (data) {
 			var html = template('teacher-view-tpl',data.result);
 			$('#teacherModal').html(html);
-			console.log(data);
 		})
-	})
+	});
 
-})
+	//讲师状态修改
+	$('#teacher-lists').on('click',".teacher-status",function () {
+		var $self = $(this);
+		$.ajax({
+			url:'/v6/teacher/handle',
+			type:'post',
+			data:{
+				tc_id:$(this).parent().attr('data-id'),
+				tc_status:$(this).parent().attr('data-status'),
+			},
+			success:function (data) {
+				if (data.code ==200){
+					data-status==0?'开启':'注销'
+				}
+			}
+		})
+	});
+
+});
