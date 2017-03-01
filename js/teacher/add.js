580b02bdcbaddc344076444c31c1826aa7a42f86
doudou <shaofengyuan94723@qq.com>
2017-03-02 00:54:21 +0800

    讲师的添加和编辑

--- js/teacher/add.js
+++ js/teacher/add.js
@@ -1,3 +1,49 @@
-define(['jquery'],function ($) {
-	console.log('teacher-add');
+define(['jquery','template'],function ($,template) {
+
+	//获取讲师信息（编辑）
+	var dataBianji = JSON.parse(decodeURIComponent(location.search).slice(1));
+
+
+	var tcId = dataBianji.tc_id;
+	var html = template('teacher-form-tpl',dataBianji);
+	$('.teacher-add').html(html);
+	console.log(tcId);
+
+	$('#teacher-add-form').on('submit',"[type='submit']",function (e) {
+		console.log(23);
+		// if(tcId){
+		// 	//编辑讲师
+		// 	console.log($('#teacher-add-form').serialize() + '&tc_id=' + dataBianji.tc_id);
+		// 	$.get('/v6/teacher/update',$('#teacher-add-form').serialize()+'&tc_id='+dataBianji.tc_id,function (data) {
+		// 		if (data.code == 200){
+		// 			location.href = '/html/teacher/teacher_list.html'
+		// 		}
+		// 	})
+		// }else{
+		// 	var html = template('teacher-form-tpl',{});
+		// 	$('.teacher-add').html(html);
+		// }
+		return false;
+	})
+
+
+
+
+
+	//添加讲师
+	$('#teacher-add-form').on('submit',function () {
+		$.ajax({
+			url:'/v6/teacher/add',
+			type:'post',
+			data:$(this).serialize(),
+			success:function (data) {
+				location.href = '/html/teacher/teacher_list.html';
+			}
+		})
+		return false;
+	})
+
+
+
+
 })
--- js/teacher/list.js
+++ js/teacher/list.js
@@ -1,12 +1,22 @@
-define(['jquery','template','common'],function ($,template,undefined) {
+define(['jquery','template','common','nprogress'],function ($,template,undefined,nprogress) {
+
+	//加载
+	nprogress.done();
 
 	//渲染讲师列表
-	$.get('/v6/teacher',function (data) {
+	$.get("/v6/teacher",function (data) {
 		if (data.code==200){
 			var html = template('teacher-list-tpl',data);
 			$('#teacher-lists').html(html);
 		}
 	});
+
+	//编辑讲师
+	$('#teacher-lists').on('click',".teacher-bianji",function () {
+		$.get('/v6/teacher/edit',{tc_id:$(this).parent().attr('data-id')},function (data) {
+			location.href = '/html/teacher/teacher_add.html?'+encodeURIComponent(JSON.stringify(data.result));
+		})
+	})
 
 	//然后获取讲师详细信息并展示。
 	$('#teacher-lists').on('click',".teacher-view",function () {
@@ -17,7 +27,7 @@ define(['jquery','template','common'],function ($,template,undefined) {
 	});
 
 	//讲师状态修改
-	$('#teacher-lists').on('click',".teacher-status",function () {
+	$('#teacher-lists').on('click',".teacher-status",function (e) {
 		var $self = $(this);
 		$.ajax({
 			url:'/v6/teacher/handle',
@@ -28,7 +38,8 @@ define(['jquery','template','common'],function ($,template,undefined) {
 			},
 			success:function (data) {
 				if (data.code ==200){
-					data-status==0?'开启':'注销'
+					$self.html(data.result.tc_status == 0?'开启':'注销');
+					$self.parent().attr('data-status',data.result.tc_status);
 				}
 			}
 		})
