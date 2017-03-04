4baf88ca86e503de6e405dbf0c0a7bbabe86d476
doudou <shaofengyuan94723@qq.com>
2017-03-04 10:12:08 +0800

    密码修改

--- js/home/repass.js
+++ js/home/repass.js
@@ -1,3 +1,16 @@
-define(['jquery'],function ($) {
-	console.log('home-repass');
+define(['jquery','common'],function ($,undefined) {
+
+	$('#repass-form').on('submit',function () {
+		$.ajax({
+			url:'/v6/teacher/repass',
+			type:'post',
+			data:$('#repass-form').serialize(),
+			success:function (data) {
+				if (data.code == 200){
+					$('#logout').trigger('click');//替我点击了退出按钮
+				}
+			}
+		});
+		return false;
+	})
 })
